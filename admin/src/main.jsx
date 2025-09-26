88bf6313cb905ed1b7c67e6c3926c759515601e1
Anurag-Basuri <anuragbasuri2502@gmail.com>
2025-09-26 12:20:25 +0530

    feat: add authentication context providers for admin and department users

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -4,11 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import './App.css';
+import { AdminAuthProvider, DepartmentAuthProvider } from './store/auth';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AdminAuthProvider>
+        <DepartmentAuthProvider>
+          <App />
+        </DepartmentAuthProvider>
+      </AdminAuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
